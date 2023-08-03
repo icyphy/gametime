9987df8d296bf42b26744050c9633afb331bf7c8
Colin Cai <colincai09@gmail.com>
2023-08-02 23:21:19 -0700

    fix format in path_generator

--- src/path_generator.py
+++ src/path_generator.py
@@ -15,13 +15,11 @@ import time
 
 import nx_helper
 import pulp_helper
-import clang_helper
 
 from defaults import logger
 from gametime_error import GameTimeError
 from nx_helper import Dag
 from path import Path
-from simulator.flexpret_simulator import flexpret_simulator
 
 
 class PathType(object):
@@ -43,16 +41,16 @@ class PathType(object):
     ALL_INCREASING = 4
 
     @staticmethod
-    def get_description(pathType):
+    def get_description(path_type):
         """
         Returns:
             One-word description of the path type provided.
         """
-        return ("worst" if pathType is PathType.WORST_CASE else
-                "best" if pathType is PathType.BEST_CASE else
-                "random" if pathType is PathType.RANDOM else
-                "all-dec" if pathType is PathType.ALL_DECREASING else
-                "all-inc" if pathType is PathType.ALL_INCREASING else "")
+        return ("worst" if path_type is PathType.WORST_CASE else
+                "best" if path_type is PathType.BEST_CASE else
+                "random" if path_type is PathType.RANDOM else
+                "all-dec" if path_type is PathType.ALL_DECREASING else
+                "all-inc" if path_type is PathType.ALL_INCREASING else "")
 
 
 class PathGenerator(object):
@@ -68,18 +66,18 @@ class PathGenerator(object):
     """
 
     @staticmethod
-    def generate_paths(analyzer, numPaths=5, pathType=PathType.WORST_CASE,
-                      interval=None, useObExtraction=False):
+    def generate_paths(analyzer, num_paths=5, path_type=PathType.WORST_CASE,
+                      interval=None, use_ob_extraction=False):
         """Generates a list of feasible paths of the code being analyzed,
         each represented by an object of the ``Path`` class.
 
-        The type of the generated paths is determined by the pathType
+        The type of the generated paths is determined by the path_type
         argument, which is a class variable of the ``PathType`` class.
         By default, this argument is ``PathType.WORST_CASE``. For
         a description of the types, refer to the documentation of
         the ``PathType`` class.
 
-        The ``numPaths`` argument is an upper bound on how many paths should
+        The ``num_paths`` argument is an upper bound on how many paths should
         be generated, which is 5 by default. This argument is ignored if
         this method is used to generate all of the feasible paths of the code
         being analyzed.
@@ -102,9 +100,9 @@ class PathGenerator(object):
             analyzer:
                 ``Analyzer`` object that maintains information about
                 the code being analyzed.
-            numPaths:
+            num_paths:
                 Upper bound on the number of paths to generate.
-            pathType:
+            path_type:
                 Type of paths to generate, represented by a class variable of
                 the ``PathType`` class. The different types of paths are
                 described in the documentation of the ``PathType`` class.
@@ -113,7 +111,7 @@ class PathGenerator(object):
                 values that the generated paths can have. If no
                 ``Interval`` object is provided, the interval of values
                 is considered to be all real numbers.
-            useObExtraction:
+            use_ob_extraction:
                 Boolean value specifiying whether to use overcomplete
                 basis extraction algorithm
 
@@ -123,57 +121,57 @@ class PathGenerator(object):
         """
         paths = None
 
-        if pathType == PathType.WORST_CASE:
+        if path_type == PathType.WORST_CASE:
             logger.info("Generating %d worst-case feasible paths..." %
-                        numPaths)
+                        num_paths)
             paths = \
-            PathGenerator._generate_paths(analyzer, numPaths,
+            PathGenerator._generate_paths(analyzer, num_paths,
                                          pulp_helper.Extremum.LONGEST,
-                                         interval, useObExtraction)
-        elif pathType == PathType.BEST_CASE:
+                                         interval, use_ob_extraction)
+        elif path_type == PathType.BEST_CASE:
             logger.info("Generating %d best-case feasible paths..." %
-                        numPaths)
+                        num_paths)
             paths = \
-            PathGenerator._generate_paths(analyzer, numPaths,
+            PathGenerator._generate_paths(analyzer, num_paths,
                                          pulp_helper.Extremum.SHORTEST,
-                                         interval, useObExtraction)
+                                         interval, use_ob_extraction)
         if paths is not None:
             logger.info("%d of %d paths have been generated." %
-                        (len(paths), numPaths))
+                        (len(paths), num_paths))
             return paths
 
-        if pathType == PathType.ALL_DECREASING:
+        if path_type == PathType.ALL_DECREASING:
             logger.info("Generating all feasible paths in decreasing order "
                         "of value...")
             paths = \
             PathGenerator._generate_paths(analyzer, analyzer.dag.numPaths,
                                          pulp_helper.Extremum.LONGEST,
-                                         interval, useObExtraction)
-        elif pathType == PathType.ALL_INCREASING:
+                                         interval, use_ob_extraction)
+        elif path_type == PathType.ALL_INCREASING:
             logger.info("Generating all feasible paths in increasing order "
                         "of value...")
             paths = \
-            PathGenerator._generate_paths(analyzer, analyzer.dag.numPaths,
+            PathGenerator._generate_paths(analyzer, analyzer.dag.num_paths,
                                          pulp_helper.Extremum.SHORTEST,
-                                         interval, useObExtraction)
+                                         interval, use_ob_extraction)
         if paths is not None:
             logger.info("%d feasible paths have been generated." % len(paths))
             return paths
 
-        if pathType == PathType.RANDOM:
+        if path_type == PathType.RANDOM:
             logger.info("Generating random feasible paths...")
             paths = \
-            PathGenerator._generate_paths(analyzer, numPaths, None, interval)
+            PathGenerator._generate_paths(analyzer, num_paths, None, interval)
             logger.info("%d of %d paths have been generated." %
-                        (len(paths), numPaths))
+                        (len(paths), num_paths))
             return paths
         else:
-            raise GameTimeError("Unrecognized path type: %d" % pathType)
+            raise GameTimeError("Unrecognized path type: %d" % path_type)
 
     @staticmethod
-    def _generate_paths(analyzer, numPaths,
+    def _generate_paths(analyzer, num_paths,
                        extremum=pulp_helper.Extremum.LONGEST,
-                       interval=None, useObExtraction=False):
+                       interval=None, use_ob_extraction=False):
         """Helper static method for the ``generatePaths`` static method.
         Generates a list of feasible paths of the code being analyzed,
         each represented by an object of the ``Path`` class.
@@ -182,7 +180,7 @@ class PathGenerator(object):
             analyzer:
                 ``Analyzer`` object that maintains information about
                 the code being analyzed.
-            numPaths:
+            num_paths:
                 Upper bound on the number of paths to generate.
             extremum:
                 Type of paths to calculate (longest or shortest),
@@ -193,7 +191,7 @@ class PathGenerator(object):
                 values that the generated paths can have. If
                 no ``Interval`` object is provided, the interval of values
                 is considered to be all real numbers.
-            useObExtraction:
+            use_ob_extraction:
                 Boolean value specifiying whether to use overcomplete
                 basis extraction algorithm
 
@@ -207,46 +205,46 @@ class PathGenerator(object):
             return []
 
         logger.info("")
-        startTime = time.perf_counter()
+        start_time = time.perf_counter()
 
-        if useObExtraction:
-            beforeTime = time.perf_counter()
+        if use_ob_extraction:
+            before_time = time.perf_counter()
             logger.info("Using the new algorithm to extract the longest path")
             logger.info("Finding Least Compatible Delta")
-            muMax = pulp_helper.find_least_compatible_mu_max(
+            mu_max = pulp_helper.find_least_compatible_mu_max(
                 analyzer, analyzer.basisPaths)
             logger.info("Found the least mu_max compatible with measurements: "
                         "%.2f in %.2f seconds" %
-                        (muMax, time.perf_counter()- beforeTime))
-            analyzer.inferredMuMax = muMax
+                        (mu_max, time.perf_counter()- before_time))
+            analyzer.inferred_mu_max = mu_max
 
-            beforeTime = time.perf_counter()
+            before_time = time.perf_counter()
             logger.info("Calculating error bounds in the estimate")
-            analyzer.errorScaleFactor, path, ilpProblem = \
+            analyzer.error_scale_factor, path, ilp_problem = \
                 pulp_helper.find_worst_expressible_path(
                     analyzer, analyzer.basisPaths, 0)
             logger.info(
                 "Total maximal error in estimates is 2 x %.2f x %.2f = %.2f" %
-                (analyzer.errorScaleFactor, muMax,
-                2 * analyzer.errorScaleFactor * muMax))
-            logger.info("Calculated in %.2f ms" % (time.perf_counter()- beforeTime))
+                (analyzer.errorScaleFactor, mu_max,
+                2 * analyzer.errorScaleFactor * mu_max))
+            logger.info("Calculated in %.2f ms" % (time.perf_counter()- before_time))
         else:
             analyzer.estimate_edge_weights()
 
-        resultPaths = []
-        currentPathNum, numPathsUnsat, numCandidatePaths = 0, 0, 0
-        while (currentPathNum < numPaths and
-               numCandidatePaths < analyzer.dag.numPaths):
+        result_paths = []
+        current_path_num, num_paths_unsat, num_candidate_paths = 0, 0, 0
+        while (current_path_num < num_paths and
+               num_candidate_paths < analyzer.dag.num_paths):
             logger.info("Currently generating path %d..." %
-                        (currentPathNum+1))
+                        (current_path_num+1))
             logger.info("So far, %d candidate paths were found to be "
-                        "unsatisfiable." % numPathsUnsat)
+                        "unsatisfiable." % num_paths_unsat)
 
             if analyzer.pathDimension == 1:
-                warnMsg = ("Basis matrix has dimensions 1x1. "
+                warn_msg = ("Basis matrix has dimensions 1x1. "
                            "There is only one path through the function "
                            "under analysis.")
-                logger.warn(warnMsg)
+                logger.warning(warn_msg)
 
             logger.info("Finding a candidate path using an integer "
                         "linear program...")
@@ -254,59 +252,59 @@ class PathGenerator(object):
 
             if extremum is None:
                 source, sink = analyzer.dag.source, analyzer.dag.sink
-                candidatePathNodes = nx_helper.get_random_path(analyzer.dag,
+                candidate_path_nodes = nx_helper.get_random_path(analyzer.dag,
                                                             source, sink)
-                candidatePathEdges = Dag.get_edges(candidatePathNodes)
-                analyzer.add_path_bundled_constraint(candidatePathEdges)
+                candidate_path_edges = Dag.get_edges(candidate_path_nodes)
+                analyzer.add_path_bundled_constraint(candidate_path_edges)
 
-            if useObExtraction:
-                candidatePathNodes, ilpProblem = \
+            if use_ob_extraction:
+                candidate_path_nodes, ilp_problem = \
                     pulp_helper.find_longest_path_with_delta(
-                        analyzer, analyzer.basisPaths, muMax, extremum)
+                        analyzer, analyzer.basisPaths, mu_max, extremum)
             else:
-                candidatePathNodes, ilpProblem = \
+                candidate_path_nodes, ilp_problem = \
                     pulp_helper.find_extreme_path(analyzer,
                                                extremum if extremum is not None
                                                else pulp_helper.Extremum.LONGEST,
                                                interval)
             logger.info("")
 
-            if ilpProblem.objVal is None:
+            if ilp_problem.obj_val is None:
                 if (extremum is not None or
-                    numCandidatePaths == analyzer.dag.numPaths):
+                    num_candidate_paths == analyzer.dag.num_paths):
                     logger.info("Unable to find a new candidate path.")
                     break
                 elif extremum is None:
-                    analyzer.add_path_exclusive_constraint(candidatePathEdges)
+                    analyzer.add_path_exclusive_constraint(candidate_path_nodes)
                     analyzer.reset_path_bundled_constraints()
-                    numCandidatePaths = len(analyzer.pathExclusiveConstraints)
+                    num_candidate_paths = len(analyzer.path_exclusive_constraints)
                     continue
 
             logger.info("Candidate path found.")
             logger.info("Running the candidate path to check feasibility and measure run time")
-            candidatePathEdges = Dag.get_edges(candidatePathNodes)
-            candidatePathValue = ilpProblem.objVal
-            resultPath = Path(ilp_problem=ilpProblem, nodes=candidatePathNodes)
+            candidate_path_edges = Dag.get_edges(candidate_path_nodes)
+            candidate_path_value = ilp_problem.obj_val
+            result_path = Path(ilp_problem=ilp_problem, nodes=candidate_path_nodes)
 
-            value = analyzer.measure_path(resultPath, f'feasible-path{currentPathNum}')
+            value = analyzer.measure_path(result_path, f'feasible-path{current_path_num}')
 
             #TODO: replace with actual value of infeasible path
             if value < float('inf'):
                 logger.info("Candidate path is feasible.")
-                resultPath.set_measured_value(value)
-                resultPath.set_predicted_value(candidatePathValue)
-                resultPaths.append(resultPath)
-                logger.info("Path %d generated." % (currentPathNum+1))
-                analyzer.add_path_exclusive_constraint(candidatePathEdges)
-                currentPathNum += 1
-                numPathsUnsat = 0
+                result_path.set_measured_value(value)
+                result_path.set_predicted_value(candidate_path_value)
+                result_paths.append(result_path)
+                logger.info("Path %d generated." % (current_path_num+1))
+                analyzer.add_path_exclusive_constraint(candidate_path_edges)
+                current_path_num += 1
+                num_paths_unsat = 0
             else:
                 logger.info("Candidate path is infeasible.")
-                analyzer.add_path_exclusive_constraint(candidatePathEdges)
+                analyzer.add_path_exclusive_constraint(candidate_path_edges)
                 logger.info("Constraint added.")
-                numPathsUnsat += 1
+                num_paths_unsat += 1
 
-            numCandidatePaths += 1
+            num_candidate_paths += 1
             if extremum is None:
                 analyzer.reset_path_bundled_constraints()
             logger.info("")
@@ -315,5 +313,5 @@ class PathGenerator(object):
         analyzer.reset_path_exclusive_constraints()
 
         logger.info("Time taken to generate paths: %.2f seconds." %
-                    (time.perf_counter() - startTime))
-        return resultPaths
+                    (time.perf_counter() - start_time))
+        return result_paths
