2351b3ed7aef5385ae980ba99e15fee066840041
C <colincai09@gmail.com>
2024-04-08 23:35:53 -0700

    fix binary search example

--- test/x86_test/programs/binarysearch/binarysearch.c
+++ test/x86_test/programs/binarysearch/binarysearch.c
@@ -4,48 +4,49 @@
 #include <unistd.h>
 #include <stdint.h>
 
-int binarysearch_binary_search(uint8_t binarysearch_data [15], uint8_t x )
+int binarysearch_binary_search(int binarysearch_data [15], int x )
 {
-  uint8_t fvalue, mid, up, low;
+  int fvalue, mid, up, low;
 
   low = 0;
   up = 14;
   fvalue = -1;
 
-  #pragma unroll 4
-  for (int i = 0; i < 4; i++) {
-    if (low <= up) {
-      mid = ( low + up ) >> 1;
+  // #pragma unroll 4
+  // for (int i = 0; i < 4; i++) {
+  //   if (low <= up) {
+  //     mid = ( low + up ) >> 1;
     
-      if ( binarysearch_data[ mid ] == x ) {
-        /* Item found */
-        up = low - 1;
-        fvalue = binarysearch_data[ mid ];
-      } else {
-          if ( binarysearch_data[ mid ] > x )
-            /* Item not found */
-            up = mid - 1;
-          else
-            low = mid + 1;
-      }
-    }
-  
-  }
-  // while (low <= up) {
-  //   mid = ( low + up ) >> 1;
-    
-  //   if ( binarysearch_data[ mid ] == x ) {
-  //     /* Item found */
-  //     up = low - 1;
-  //     fvalue = binarysearch_data[ mid ];
-  //   } else {
-  //       if ( binarysearch_data[ mid ] > x )
+  //     if ( binarysearch_data[ mid ] == x ) {
+  //       /* Item found */
+  //       up = low - 1;
+  //       fvalue = binarysearch_data[ mid ];
+  //     } else {
+  //         if ( binarysearch_data[ mid ] > x )
   //           /* Item not found */
   //           up = mid - 1;
-  //       else
+  //         else
   //           low = mid + 1;
+  //     }
   //   }
+  
   // }
+  #pragma unroll 4
+  while (low <= up) {
+    mid = ( low + up ) >> 1;
+    
+    if ( binarysearch_data[ mid ] == x ) {
+      /* Item found */
+      up = low - 1;
+      fvalue = binarysearch_data[ mid ];
+    } else {
+        if ( binarysearch_data[ mid ] > x )
+            /* Item not found */
+            up = mid - 1;
+        else
+            low = mid + 1;
+    }
+  }
 
   return fvalue;
 }
