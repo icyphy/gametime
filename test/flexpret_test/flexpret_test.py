3c3c2e632c8a11cdfad7e33c0a31653b75172893
C <colincai09@gmail.com>
2024-04-29 22:35:49 -0700

    update test

--- test/flexpret_test/flexpret_test.py
+++ test/flexpret_test/flexpret_test.py
@@ -24,9 +24,6 @@ class TestFlexpret(unittest.TestCase):
         paths = analyzer.generate_basis_paths()
 
         self.assertIsNotNone(paths[0], "no paths were found")
-        #print([analyzer.dag.get_node_label(analyzer.dag.nodes_indices[node])  for path in paths for node in path.nodes])
-        # extract bitcode from path
-        #print(len(paths))
         output_dir = os.path.join(self.project_config.location_temp_dir, "output")
         os.mkdir(output_dir)
         labels = []
@@ -36,18 +33,6 @@ class TestFlexpret(unittest.TestCase):
                 bitcode.append(analyzer.dag.get_node_label(analyzer.dag.nodes_indices[node]))
             labels_file = find_labels("".join(bitcode), output_dir)
             run_smt(self.project_config, labels_file, output_dir)
-
-
-        # path_analyzer = PathAnalyzer(analyzer.preprocessed_path, analyzer.project_config, analyzer.dag, paths[0], "path0")
-        # output_file = path_analyzer.change_bt_based_on_path()
-        # print(output_file)
-        
-        # self.assertIsNotNone(output_file)
-        # self.assertTrue(len(output_file) != 0)
-        # self.assertTrue(os.path.isfile(output_file))
-
-        #analyzer.measure_basis_paths()
-
 
 
 if __name__ == '__main__':
