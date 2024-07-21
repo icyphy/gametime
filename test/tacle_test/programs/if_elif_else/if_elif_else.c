1226c6ecf3d3d79de3e722fadef4fb09ac808d73
Abdalla <a.eltayeb@campus.tu-berlin.de>
2024-07-21 15:05:59 +0200

    make test easier

--- test/tacle_test/programs/if_elif_else/if_elif_else.c
+++ test/tacle_test/programs/if_elif_else/if_elif_else.c
@@ -2,25 +2,30 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-int test (int x, int b) {
-   if (x < 0 && b < 0) {
-      // sleep(5);
+int test (int x) {
+   // if (x < 0 && b < 0) {
+   //    // sleep(5);
+   //    return 1;
+   // } else if (x < 0 && b == 0) {
+   //    return 2;
+   // } else if (x < 0 && b > 0) {
+   //    return 3;
+   // } else if (x == 0 && b < 0) {
+   //    return 4;
+   // // } else if (x == 0 && b == 0) {
+   // //    return 5;
+   // // } else if (x == 0 && b > 0) {
+   // //    return 6;
+   // // } else if (x > 0 && b < 0) {
+   // //    return 7;
+   // // } else if (x > 0 && b == 0) {
+   // //    return 8;
+   // } else {
+   //    return 9;
+   // }
+   if(x > 0) {
       return 1;
-   } else if (x < 0 && b == 0) {
-      return 2;
-   } else if (x < 0 && b > 0) {
-      return 3;
-   } else if (x == 0 && b < 0) {
-      return 4;
-   // } else if (x == 0 && b == 0) {
-   //    return 5;
-   // } else if (x == 0 && b > 0) {
-   //    return 6;
-   // } else if (x > 0 && b < 0) {
-   //    return 7;
-   // } else if (x > 0 && b == 0) {
-   //    return 8;
    } else {
-      return 9;
+      return 0;
    }
 }
